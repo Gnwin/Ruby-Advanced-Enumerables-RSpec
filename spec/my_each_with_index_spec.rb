048ee336f53cbc2bed02429f0294316074a50236
Godwin Nwachukwu <nwgwin@gmail.com>
2021-04-06 18:39:07 +0100

    write tests for my each with index

--- spec/my_each_with_index_spec.rb
+++ spec/my_each_with_index_spec.rb
@@ -2,27 +2,35 @@ require './lib/enumerables'
 
 RSpec.describe Enumerable do
   let(:array_of_numbers) { [1, 2, 3, 4, 5] }
-  let(:str_array) { %w[the sumerians ruled] }
-  let(:my_hash) { { obj: 'Object', obj1: 'Object1' } }
+  # let(:str_array) { %w[the sumerians ruled] }
+  # let(:my_hash) { { obj: 'Object', obj1: 'Object1' } }
   let(:my_results) { [] }
-  let(:my_proc) { proc { |item| item * 2 } }
+  # let(:my_proc) { proc { |item| item * 2 } }
 
   describe '#my_each_with_index' do
     context 'when a block is passed' do
-      it 'runs the block and returns the calling array' do
-        expect(array_of_numbers.my_each_with_index{ |item, index| puts `I am #{item} and my index is #{index}` }).to eql(array_of_numbers)
+      it 'returns the calling array' do
+        arr1 = array_of_numbers.my_each_with_index do |item, index| 
+          puts item
+          puts index
+        end
+        expect(arr1).to eql(array_of_numbers)
       end
     end
 
     context 'when a block is passed' do
-      it 'runs the block passed and does not return an empty array' do
-        expect(array_of_numbers.my_each{ |num| puts num }).to_not eql(my_results)
+      it 'returns the calling array' do
+        arr1 = array_of_numbers.my_each_with_index do |item, index| 
+          puts item
+          puts index
+        end
+        expect(arr1).to_not eql(my_results)
       end
     end
 
     context 'when a block is not passed' do
-      it 'runs the block passed and returns the calling array' do
-        expect(array_of_numbers.my_each).to be_an(Enumerator)
+      it 'returns an enumerator' do
+        expect(array_of_numbers.my_each_with_index).to be_an(Enumerator)
       end
     end
   end
