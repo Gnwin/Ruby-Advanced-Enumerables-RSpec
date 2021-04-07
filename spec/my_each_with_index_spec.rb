c95eb963df3dc048bf10553d60f060716bdefd02
Godwin Nwachukwu <nwgwin@gmail.com>
2021-04-07 03:10:58 +0100

    refactor result_arr in my_each_with_index_spec.rb

--- spec/my_each_with_index_spec.rb
+++ spec/my_each_with_index_spec.rb
@@ -3,24 +3,22 @@ require './lib/enumerables'
 RSpec.describe Enumerable do
   let(:array_of_numbers) { [1, 2, 3, 4, 5] }
   let(:my_results) { [] }
+  let(:result_arr) do
+    array_of_numbers.my_each_with_index do |item, index|
+      puts item
+      puts index
+    end
+  end
 
   describe '#my_each_with_index' do
     context 'when a block is passed' do
       it 'returns the calling array' do
-        result_arr = array_of_numbers.my_each_with_index do |item, index|
-          puts item
-          puts index
-        end
         expect(result_arr).to eql(array_of_numbers)
       end
     end
 
     context 'when a block is passed' do
       it 'returns the calling array' do
-        result_arr = array_of_numbers.my_each_with_index do |item, index|
-          puts item
-          puts index
-        end
         expect(result_arr).to_not eql(my_results)
       end
     end
